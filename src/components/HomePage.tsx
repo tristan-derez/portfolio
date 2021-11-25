9657ad62ee6d8bc4711b2780fd8ed888f5bf7533
dreyzu <tristan.derez@gmail.com>
2021-11-25 13:01:31 +0100

    create HomePage

---
+++ src/components/HomePage.tsx
@@ -0,0 +1,17 @@
+import { Flex } from "@chakra-ui/react";
+
+import { AboutMeContainer } from "./aboutme";
+import { FooterComponent } from "./footer";
+import { HeaderComponent } from "./header";
+import { ProjectContainer } from "./projects";
+
+export const HomePage = () => {
+    return (
+        <Flex direction="column">
+            <HeaderComponent />
+            <AboutMeContainer />
+            <ProjectContainer />
+            <FooterComponent />
+        </Flex>
+    );
+};
