0c60ea02d9a643a0df971bf4fb9dfec6c347c147
dreyzu <tristan.derez@gmail.com>
2021-11-25 14:18:25 +0100

    add nomatch component

---
+++ src/components/nomatch.tsx
@@ -0,0 +1,12 @@
+import { Button, Flex, Image, Link } from "@chakra-ui/react";
+
+const orange = "#F98227";
+
+export const NoMatch = () => (
+    <Flex direction="column" alignItems="center" justifyContent="center" minHeight="100vh">
+        <Image src="/images/undraw_page_not_found.svg" />
+        <Button color={orange} mt="50px">
+            <Link href="/">Go back to Home Page</Link>
+        </Button>
+    </Flex>
+);
