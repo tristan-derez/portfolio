5227c188f505c13fcafc77d4a4c9b51a543563a3
dreyzu <tristan.derez@gmail.com>
2022-02-17 19:11:39 +0100

    update imports

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,10 @@ import { QueryClient, QueryClientProvider } from "react-query";
 import { Link, Route, Routes } from "react-router-dom";
 import { BrowserRouter } from "react-router-dom";
 
-import { Contact } from "./components/contact";
+import { Contact } from "./components/Contact";
 import { Cursor } from "./components/Cursor";
+import { NoMatch } from "./components/Error";
 import { HomePage } from "./components/HomePage";
-import { NoMatch } from "./components/nomatch";
 
 const queryClient = new QueryClient();
 
