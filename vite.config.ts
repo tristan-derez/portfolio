842d4d38e855febd4b3b191b9fe2443b47a77f07
dreyzu <tristan.derez@gmail.com>
2022-02-17 18:15:41 +0100

    build: add src to base

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: "/",
+    base: "/src",
     root: "./",
     build: { outDir: "./dist", sourcemap: true },
     plugins: [
